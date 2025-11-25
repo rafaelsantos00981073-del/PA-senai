bd1d4563c56b0f6a6a198003d230d1d5327ab47b
rafaelsantos00981073-del <rafael.santos00981073@sesisenaipr.org.br>
2025-11-25 17:15:56 -0300

    Add files via upload

---
+++ desafio/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$password = "root";
+$database = "biblioteca";
+$port = 3307;
+
+$conn = mysqli_connect($server, $user, $password, $database, $port);
+
+if (!$conn) {
+    die("Connection error: " . mysqli_connect_error());
+}
+?>
